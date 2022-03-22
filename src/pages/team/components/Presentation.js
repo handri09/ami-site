051e1d7b2bcf0c8056bddf1cc8ab636d4c68605e
Hajaniaina <piadmin@toamasina.local>
2022-03-22 21:17:32 +0300

    [code] Updated Presentation page

--- src/pages/team/components/Presentation.js
+++ src/pages/team/components/Presentation.js
@@ -1,15 +1,29 @@
-import React from "react"
+import React, { useEffect } from "react"
+import gp1 from "../../../assets/teams/gp1.JPG"
+import gp2 from "../../../assets/teams/gp2.jpg"
+import slidel from "../../../assets/slide_left.png"
+import slider from "../../../assets/slide_right.png"
+
+const images = {
+  1: gp1,
+  2: gp2,
+}
 
 function Presentation () {
-  const [state, setState] = React.useState({
-    content: [],
-  })
-
+  const [count, setCount] = React.useState(1);
+  const onPrev = () => {
+    setCount(count => count-1)
+  }
+  const onNext = () => {
+    setCount(count => count+1)
+  }
   return (
-    <div className="flex justify-center items-center border border-red-600 gap-3 h-96"> 
-      <div onClick={() => console.log("prev")}>slide gauche</div>
-      <div className="w-1/2 bg-green-100">image</div>
-      <div onClick={() => console.log("next")}>slide droite</div>
+    <div className="flex justify-center items-center gap-3 shadow-md my-10"> 
+      <div><button onClick={onPrev} disabled={count===1 ? true : false }><img src={slidel} /></button></div>
+      <div className="flex flex-col w-1/2 bg-green-100">
+        <img src={images[count]} />
+      </div>
+      <div><button onClick={onNext} disabled={count===2 ? true : false }><img src={slider} /></button></div>
     </div>
   );
 }
