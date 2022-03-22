c211aad4320f254311e335aa64861b037affe284
Hajaniaina <piadmin@toamasina.local>
2022-03-22 16:45:08 +0300

    [code] Updated the NavBar and Apps for taking Routes working

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,10 +5,10 @@ function NavBar() {
   return (
    <div className="antialiased flex sticky w-full top-0 z-20 h-20 shadow-md justify-end">
      <div className="flex gap-10 items-center justify-end mx-5">
-       <Link to="/" className="hover:text-green-500 hover:bg-green-100 p-3">Home</Link>
-       <Link to="/activities" className="hover:text-green-500 hover:bg-green-100 p-3">Activities</Link>
-       <Link to="/team" className="hover:text-green-500 hover:bg-green-100 p-3">Team</Link>
-       <Link to="/contact" className="hover:text-green-500 hover:bg-green-100 p-3">Contact</Link>
+       <Link to="/ami-site/" className="hover:text-green-500 hover:bg-green-100 p-3">Home</Link>
+       <Link to="/ami-site/activities" className="hover:text-green-500 hover:bg-green-100 p-3">Activities</Link>
+       <Link to="/ami-site/team" className="hover:text-green-500 hover:bg-green-100 p-3">Team</Link>
+       <Link to="/ami-site/contact" className="hover:text-green-500 hover:bg-green-100 p-3">Contact</Link>
      </div>
    </div>
   );
