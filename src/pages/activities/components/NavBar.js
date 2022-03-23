03912f13451ed9ddfeee8ac3d97407d55b34d2cf
Hajaniaina <piadmin@toamasina.local>
2022-03-23 19:47:28 +0300

    [code] Update

--- src/pages/activities/components/NavBar.js
+++ src/pages/activities/components/NavBar.js
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom"
 
 function NavBar() {
   return (
-   <div className="antialiased flex sticky w-full top-0 z-20 h-20 shadow-md justify-end">
+   <div className="antialiased flex sticky w-full top-0 z-20 h-20 shadow-md justify-end text-xl">
      <div className="flex gap-10 items-center justify-end mx-5">
        <Link to="/ami-site/" className="hover:text-green-500 hover:bg-green-100 p-3 px-5">Home</Link>
        <Link to="/ami-site/activities" className="text-green-500 border border-green-500 rounded-md p-3 px-5">Activities</Link>
