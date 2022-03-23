cbdb4fd20afa5173beb6dc8c3c8eb1900a0f31c8
Hajaniaina <piadmin@toamasina.local>
2022-03-23 21:01:54 +0300

    [code] Updating the NavBar

--- src/pages/team/components/NavBar.js
+++ src/pages/team/components/NavBar.js
@@ -1,16 +1,27 @@
 import React from "react"
 import {Link} from "react-router-dom"
+import logo from "../../../assets/LOGO_.png"
+
+const styling = "hover:border-green-500 hover:text-green-500 border-white border-2 rounded-md p-3 px-5"
+const opened = "border-green-500 text-green-500 border-2 rounded-md p-3 px-5"
 
 function NavBar() {
   return (
-   <div className="antialiased flex sticky bg-white w-full top-0 z-20 h-20 shadow-md justify-end">
-     <div className="flex gap-10 items-center justify-end mx-5 font-mono text-xl">
-       <Link to="/ami-site/" className="hover:text-green-500 hover:bg-green-100 p-3 px-5">Home</Link>
-       <Link to="/ami-site/activities" className="hover:text-green-500 hover:bg-green-100 p-3 px-5">Activities</Link>
-       <Link to="/ami-site/team" className="text-green-500 border border-green-500 rounded-md p-3 px-5">Team</Link>
-       <Link to="/ami-site/contact" className="hover:text-green-500 hover:bg-green-100 p-3 px-5">Contact</Link>
-     </div>
-   </div>
+    <div className={`
+      flex antialiased sticky 
+      w-full top-0 h-20 z-20 text-xl font-mono 
+      shadow-md justify-end`}>
+
+      <div className="flex w-1/2 px-10"><img className="" src={logo} /></div>
+
+      <div className="flex w-1/2 gap-16 items-center justify-center">
+        <Link to="/ami-site/" className={styling}>Home</Link>
+        <Link to="/ami-site/activities" className={styling}>Activities</Link>
+        <Link to="/ami-site/team" className={opened}>Team</Link>
+        <Link to="/ami-site/contact" className={styling}>Contact</Link>
+      </div>
+
+    </div>
   );
 }
 
