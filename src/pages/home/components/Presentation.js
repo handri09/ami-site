b17b76fa634a3ef28ba7ef12548f717c90674229
Hajaniaina <piadmin@toamasina.local>
2022-03-24 09:43:47 +0300

    [code] Add Title to the Video

--- src/pages/home/components/Presentation.js
+++ src/pages/home/components/Presentation.js
@@ -4,8 +4,9 @@ import {AspectRatio} from '@chakra-ui/react'
 function Presentation() {
   return (
     <>
-      <div className="flex justify-center items-center h-screen w-screen bg-green-200 py-24 -mt-20">
-        <div className="w-full h-full p-5">
+      <div className="flex justify-center items-center h-screen w-full py-24 -mt-20">
+        <div className="w-full h-full">
+          <h1 className="m-5 font-bold text:text-2xl lg:text-4xl font-mono text-green-600">Mr. Léonce, AMI's president.</h1>
           <AspectRatio ratio={1}>
             <iframe
               title='Léonce'
@@ -17,7 +18,7 @@ function Presentation() {
         </div>
       </div>
       <div className="h-screen w-full bg-red-100">
-
+        
       </div>
     </>
   )
