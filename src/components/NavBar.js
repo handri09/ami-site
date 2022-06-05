056ee32edec40289aeeffeb554a45c811b62bfee
Hajaniaina Andrianavalona <handri09@ambatovy.mg>
2022-06-05 21:34:36 +0300

    Reorganise The Home Page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,32 @@
-import React from "react"
-import {Link} from "react-router-dom"
-import logo from "../assets/LOGO_.png"
-import { Outlet } from "react-router-dom";
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
+import logo from '../assets/LOGO_.png';
 
-const styling = "hover:border-green-500 hover:text-green-500 border-white border-2 rounded-md p-3 px-5 font-bold "
-const opened = "border-green-500 text-green-500 border-white border-2 rounded-md p-3 px-5"
+const styling = 'hover:border-green-500 hover:text-green-500 border-white border-b-2 p-1 md:p-3 px-5'
 
 function NavBar() {
   return (
     <>
-      <div className={`w-full top-0 flex flex-col md:flex-row antialiased justify-end sticky text-xl lg:text-2xl font-mono shadow-md bg-white z-10`}>
-        <Link to="/ami-site/" className="flex pl-5 md:pl-10 w-full md:w-1/3 xl:w-1/2 h-20"><img className={`h-20`} src={logo} alt={`ami`}/></Link>
-        <div className="flex flex-col md:flex-row w-full md:w-2/3 xl:w-1/2 gap-2 md:gap-3 lg:gap-5 xl:gap-10 items-center justify-center">
-          <Link to="/ami-site/" className={styling}>Home</Link>
-          <Link to="/ami-site/activities" className={styling}>Activities</Link>
-          <Link to="/ami-site/team" className={styling}>Team</Link>
-          <Link to="/ami-site/contact" className={styling}>Contact</Link>
+      <div
+        className={`
+          w-full top-0 flex flex-col md:flex-row 
+          antialiased justify-end sticky text-xl
+          2xl:text-2xl shadow-md bg-white z-10
+          `}>
+
+        <Link
+          to='/ami-site/'
+          className={`flex pl-5 md:pl-10 w-full md:w-1/3 xl:w-1/2 h-20`}>
+          <img className={`h-20`} src={logo} alt={`ami`} />
+        </Link>
+
+        <div className='flex flex-col md:flex-row w-full md:w-2/3 xl:w-1/2 gap-2 md:gap-3 lg:gap-5 xl:gap-10 items-center justify-center'>
+          <Link to='/ami-site/' className={styling}>Home</Link>
+          <Link to='/ami-site/activities' className={styling}>Activities</Link>
+          <Link to='/ami-site/team' className={styling}>Team</Link>
+          <Link to='/ami-site/contact' className={styling}>Contact</Link>
         </div>
+        
       </div>
       <Outlet />
     </>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,56 @@
-import React from "react";
-import {Presentation} from "./components"
+import React from 'react';
+import { Presentation, Footer } from '../../components';
+import {AspectRatio} from '@chakra-ui/react';
 
 function Home() {
   return (
     <>
-      <Presentation />
+      <div className={`
+        w-full flex
+        gap-2
+        text-2xl text-gray-700
+        `}>
+          <div className={`w-1/4 h-screen bg-green-50`}>
+            <h1 className={`flex justify-center items-center p-2`}>Contact</h1>
+          </div>
+
+          <div className={`w-2/4 flex flex-col gap-3 h-screen bg-red-50 text-2xl`}>
+            <h1 className={`text-center p-3 font-bold`}>Mr. Léonce, AMI's president.</h1>
+            <AspectRatio ratio={1} className={`flex w-full border border-white`} style={{ maxHeight:"50vh" }}>
+              <iframe
+                title='Léonce'
+                src='https://www.youtube.com/embed/QMyEdgGEfIg'
+                allowFullScreen
+              />
+            </AspectRatio>
+            <div className={`w-full flex flex-col text-justify p-10 gap-3`}>
+              <p className="">
+                AMI is an association of young people living at Ambohitsilaozana in Madagascar.
+                The association aim to help the kids in needs whether in terms of scholarship or schools equipments. 
+                One of the reason why AMI is created is, many children drop out due to familly issue, sometimes lack motivation or not enough many to pursue the school.
+                Then AMI was created to help those kids with what we have.
+              </p>
+              <p className="">If you may help, below is the IBAN</p>
+              <p className="">         
+                <code className='text-gray-800'>IBAN: MG 46 00005 0001 3729 5063 0001 80<br />
+                Domiciliation: AMBATONDRAZAKA BNI <br />
+                Titulaire: ASSOCIATION AMI - ANKIZY MIVOATRA NO IFANDROMBONANA <br /></code> 
+                <span className='flex justify-center font-mono text-gray-800 py-5'>***BIC CLMDMGMG***</span>
+              </p>
+            </div>
+          </div>
+
+          <div className={`w-1/4 flex flex-col gap-1 p-2 h-full pb-5 bg-gray-300 text-xl`}>
+            <h1 className={`flex justify-center items-center p-2 text-2xl font-bold`}>Actuality</h1>
+            {
+              ['One', 'Two', 'Three', 'Four'].map(actu => (
+                <div key={actu} className={`hover:transform hover:border-gray-400 border-2 hover:bg-gray-50 w-full h-24 bg-gray-100 flex items-center justify-center`}>News {actu}</div>    
+              ))
+            }
+          </div>
+      </div>
+      {/* <Presentation /> */}
+      <Footer />
     </>
   );
 }
