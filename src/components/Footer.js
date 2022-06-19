56018809bb20f48a09d738b1dc8c3c55e32e553d
Hajaniaina Andrianavalona <handri09@ambatovy.mg>
2022-06-19 17:13:33 +0300

    Updated the Text Size

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,10 @@ import React from 'react'
 function Footer() {
   return (
     <>
-      <div className={`w-full h-20 bg-violet-300`}>
-        Footer
+      <div className={`w-full h-full bg-violet-300 p-5`}>
+        <p>CONTACT</p>
+        <p>Phone: 034 27 536 51</p>
+        <p>Address: Ambohitsilaozana</p>
       </div>
     </>
   )
